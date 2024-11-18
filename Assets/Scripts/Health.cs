8153de6824437c8f2ce7b30eb92b37a0278b2463
Katya Zavyalova <82714610+KatyaZav@users.noreply.github.com>
2024-11-18 16:54:31 +0500

    add health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health
+{
+    public Action<float> HealthChanged;
+
+    private float _currentHealth;
+    private float _maxHealth;
+
+    public Health(float maxHealth)
+    {
+        _currentHealth = maxHealth;
+        _maxHealth = maxHealth;
+    }
+
+    public float CurretnHealth => _currentHealth;
+
+    public void RemoveHealth(float health)
+    {
+        _currentHealth -= health;
+       
+        if (_currentHealth <= 0)
+        {
+            _currentHealth = 0;
+            HealthChanged?.Invoke(_currentHealth);
+        }
+    }
+
+    public void AddHealth(float health)
+    {
+        _currentHealth += health;
+
+        if (_currentHealth > _maxHealth)
+        {
+            _currentHealth = _maxHealth;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,10 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     public event Action PlayerStoped;
 
+    [Header("Components")]
     [SerializeField] private InputController _inputController;
     [SerializeField] private NavMeshAgent _navMeshAgent;
     [SerializeField] private Animator _animator;
-    
+
+    [Header("Settings")]
+    [SerializeField] private float _maxHealth;
+    [SerializeField, Range(0, 1)] private float _layerChangeValue = 0.3f;
+
+    private Health _health;
     private NavigationMover _navigationMover;
     private AnimatorView _animatorView;
     private bool _previousState;
@@ -30,13 +33,34 @@ public class PlayerController : MonoBehaviour
         _animatorView.SetRunning(_navigationMover.isGoing);
     }
 
+    private void OnDestroy()
+    {
+        _inputController.LeftMouseClicked -= OnLeftMouseClicked;
+        _health.HealthChanged -= OnHealthChanged;        
+    }
+
     public void Init()
     {
         _navigationMover = new NavigationMover(_navMeshAgent);
         _animatorView = new AnimatorView(_animator);
+        _health = new Health(_maxHealth);
+
+        _navigationMover.SetIsMoving(false);
 
         _inputController.LeftMouseClicked += OnLeftMouseClicked;
-        _navigationMover.SetIsMoving(false);
+        _health.HealthChanged += OnHealthChanged;
+    }
+
+    private void OnHealthChanged(float obj)
+    {
+        if (_health.CurretnHealth / _maxHealth <= _layerChangeValue)
+        {
+            print("Смена анимации to injured");
+        }
+        else
+        {
+            print("animation change to normal");
+        }
     }
 
     private void OnLeftMouseClicked(Vector3 vector)
