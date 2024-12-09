89d005ba42b9e78f6c589d197d373b5d72478426
Baronne_Noire <party2015ru@mail.ru>
2024-12-09 17:19:54 +0500

    refactor wallet

--- Assets/Scripts/Wallet/Wallet.cs
+++ Assets/Scripts/Wallet/Wallet.cs
@@ -4,21 +4,14 @@ public class Wallet
 {
     public Wallet(Currency money, Currency diamonds, Currency energy)
     {
-        Money = money;
-        Diamonds = diamonds;
-        Energy = energy;
+        Currencies = new Dictionary<CurrenceType, Currency>();
 
-        Currencies = new List<Currency>();
-        Currencies.Add(Money);
-        Currencies.Add(Energy);
-        Currencies.Add(Diamonds);
+        Currencies[CurrenceType.money] = money;
+        Currencies[CurrenceType.diamonds] = diamonds;
+        Currencies[CurrenceType.energy] = energy;
     }
 
-    public Currency Money { get; private set; }
-    public Currency Diamonds { get; private set; }
-    public Currency Energy { get; private set; }
-
-    public List<Currency> Currencies { get; private set; }
+    public Dictionary<CurrenceType, Currency> Currencies { get; private set; }
 
 }
 
